d177f908c5b528a5237895b2c180fa1d6c8c69d8
Filip-Ericsson <filip.ericsson@elev.ga.ntig.se>
2019-10-10 13:10:06 +0200

    Finished V1

---
+++ FightSimV1/Fighter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightSimV1
+{
+    class Fighter
+    {
+        private int hp = 100;
+        public string name = "";
+        public static Random gen = new Random();
+
+        public int Attack()
+        {
+            return gen.Next(5, 20);
+        }
+
+        public void Hurt(int amount)
+        {
+            hp -= amount;
+
+            if (hp <0)
+            {
+                hp = 0;
+            }
+        } 
+
+        public bool IsAlive()
+        {
+            if (hp <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public int GetHp()
+        {
+            return hp;
+        }
+    }
+}
--- FightSimV1/Program.cs
+++ FightSimV1/Program.cs
@@ -10,6 +10,36 @@ namespace FightSimV1
     {
         static void Main(string[] args)
         {
+            Fighter FighterA = new Fighter();
+            FighterA.name = "Raynor";
+            Fighter FighterB = new Fighter();
+            FighterB.name = "Tycus";
+            
+
+
+            while (true)
+            {
+                FighterB.Hurt(FighterA.Attack());
+
+                FighterA.Hurt(FighterB.Attack());
+
+                Console.WriteLine(FighterA.name+" Hp is: " + FighterA.GetHp());
+                Console.WriteLine(FighterB.name + " Hp is: " + FighterB.GetHp());
+
+                if (FighterA.IsAlive() == false)
+                {
+                    Console.WriteLine(FighterB.name +" is our winner!");
+                    Console.ReadLine();
+                    break;
+                }
+                else if (FighterB.IsAlive() == false )
+                {
+                    Console.WriteLine(FighterA.name +" winns this battle!");
+                    Console.ReadLine();
+                    break;
+                }
+
+            }
         }
     }
 }
